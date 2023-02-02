30664ba9309d7327f8bfe479b85a425b2b8204f2
Farbod <it@persianpsychology.com>
2023-02-02 11:07:14 +0330

    a

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function(Request $request) {
         'input' => $request->ip(),
         'header' => $request->header('user-agent')
     ]);
+    dd($request->header());
     activity()->log($description);
  return 'hi';
     // return what you want
