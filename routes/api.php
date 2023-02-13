d1de978377e13798c4c8291292bdd90d61acec78
persianpsy <it@persianpsychology.com>
2023-02-13 12:58:48 +0330

    a

--- routes/api.php
+++ routes/api.php
@@ -67,7 +67,8 @@ Route::get('/welcome', function(Request $request) {
 
 //bbb
 Route::get('/bbb', function() {
-    Mail::to('partovi74@gmail.com')->send(new \App\Mail\InviteFriend ('data'));
+    $data = ['code' => '11','text'=>'22','sender'=>'partovi74@gmail.com'];
+    Mail::to('partovi74@gmail.com')->send(new \App\Mail\InviteFriend ($data ));
 //return bigbluebutton()->all();
 
 });
