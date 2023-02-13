ac52f55dd9ce0850bde3d1332c52ff2eaea5c5c7
Farbod <it@persianpsychology.com>
2023-02-13 11:44:57 +0330

    a

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -80,6 +80,7 @@ class UserRepository extends BaseController implements ShouldQueue
 
         if ($request->location != 'IR' || substr($request->cellphone,0,4) != '0098' || substr($request->cellphone,0,3) != '001')
         {
+            return 'hi';
             $user->location = LocationStatus::OUT ; // NOT IR;
 
             $info = array([
