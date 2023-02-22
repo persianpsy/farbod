487e8d5c5625060fed1d5f6f3c3324222ab09ef4
Farbod <it@persianpsychology.com>
2023-02-22 12:56:48 +0330

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -73,7 +73,7 @@ class Kernel extends ConsoleKernel
 
         })->daily();
 
-        $schedule->command('queue:work')->everyFiveMinutes();
+//        $schedule->command('queue:work')->everyFiveMinutes();
     }
 
     /**
