a8d6fbcf38581ce309b7af8d8ebc3002e2813128
Farbod <it@persianpsychology.com>
2023-02-22 17:42:15 +0330

    update sentry

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -213,7 +213,7 @@ class PaymentController extends BaseController
         curl_close($ch);
         $result = json_decode($result, true);
         $reservation = Reservation::with('wallet','user','staff')->where('payment_id',$payment->id)->first();
-        return $reservation;
+        dd($reservation);
 //        $description = serialize([
 //            'event' => 'verify Bank port ',
 //            'res'   => $result,
